9057fe4e99aac691f6eb92a18c9640f0d1b65b0e
Huan Suh <huansuh@gmail.com>
2019-12-23 22:17:56 +0900

    route settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
+import 'package:page_transition/page_transition.dart';
 import 'package:stackoverflutter/src/model/contents/contents_item.dart';
+import 'package:stackoverflutter/src/view/page/page_contents_detail.dart';
 import 'package:stackoverflutter/src/view/page/page_contents_list.dart';
 import 'package:stackoverflutter/src/view/page/page_not_found.dart';
 
 import './src/view/page/page_home.dart';
 import './src/view/page/page_signin.dart';
 import 'package:flutter/material.dart';
-import 'package:page_transition/page_transition.dart';
 
 void main() => runApp(MyApp());
 
@@ -17,18 +18,48 @@ class MyApp extends StatelessWidget {
       title: 'stackoverFlutter',
       theme: ThemeData.light(),
       initialRoute: '/',
-      routes: {
-        '/': (_) => HomePage(),
-        '/articles': (_) => ContentsListPage(ContentsType.ARTICLE),
-        '/questions': (_) => ContentsListPage(ContentsType.QUESTION),
-        '/users/signin': (_) => SignInPage(),
-      },
       onGenerateRoute: (settings) {
-        return PageTransition(
-          child: NotFoundPage(),
-          type: PageTransitionType.fade,
-        );
+        switch (settings.name) {
+          case '/':
+            return route(HomePage(), settings);
+          case '/articles':
+            return route(ContentsListPage(ContentsType.ARTICLE), settings);
+          case '/questions':
+            return route(ContentsListPage(ContentsType.QUESTION), settings);
+          case '/users/signin':
+            return route(SignInPage(), settings);
+          default:
+            if (settings.name.startsWith('/articles?id=')) {
+              return route(
+                ContentsDetailPage(ContentsType.ARTICLE, itemId: '1'),
+                settings,
+              );
+            }
+            if (settings.name.startsWith('/questions?id=')) {
+              return route(
+                ContentsDetailPage(ContentsType.QUESTION, itemId: '1'),
+                settings,
+              );
+            }
+        }
+        return route(NotFoundPage(), settings);
       },
+    );
+  }
+
+  PageTransition route(
+    Widget child,
+    RouteSettings settings, {
+    bool maintainState = true,
+  }) {
+    return PageTransition(
+      child: child,
+      settings: RouteSettings(
+        name: settings.name,
+        arguments: settings.arguments,
+        isInitialRoute: true,
+      ),
+      type: PageTransitionType.fade,
     );
   }
 }
