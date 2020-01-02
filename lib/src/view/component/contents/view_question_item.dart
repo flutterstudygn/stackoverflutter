fc2f9364e95df2941043a8e48b2fc3149be947f3
Huan Suh <huansuh@gmail.com>
2020-01-02 13:27:08 +0900

    + LimitedContentsListView
     + ArticleItemView, QuestionItemView : (dummy)

--- lib/src/view/component/contents/view_question_item.dart
+++ lib/src/view/component/contents/view_question_item.dart
@@ -1,8 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:stackoverflutter/src/model/contents/question_item.dart';
 
 class QuestionItemView extends StatelessWidget {
+  final QuestionItem _item;
+  QuestionItemView(this._item, {Key key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return InkWell(
+      onTap: _item?.id == null
+          ? null
+          : () {
+              Navigator.of(context).pushNamed('/questions?id=${_item.id}');
+            },
+      child: Container(
+        width: double.infinity,
+        child: Container(height: 80.0),
+      ),
+    );
   }
 }
