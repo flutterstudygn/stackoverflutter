bd1436ad860c5e748308fed2da9d6e9596b01b8c
Sugyo_In <sgin0821@gmail.com>
2020-01-05 03:55:40 +0900

    [Update] HomePage 리팩토링 및 firestore 연동.

--- lib/src/view/page/page_home.dart
+++ lib/src/view/page/page_home.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:stackoverflutter/src/bloc/home_bloc.dart';
-import 'package:stackoverflutter/src/model/contents/contents_item.dart';
-import 'package:stackoverflutter/src/view/component/contents/view_contents_list_limited.dart';
+import 'package:stackoverflutter/src/util/web_navigator.dart';
+import 'package:stackoverflutter/src/view/component/contents/view_article_item.dart';
+import 'package:stackoverflutter/src/view/component/contents/view_minimized_contents_list.dart';
+import 'package:stackoverflutter/src/view/component/contents/view_question_item.dart';
+import 'package:stackoverflutter/src/view/component/view_panel_header.dart';
+import 'package:stackoverflutter/src/view/page/page_contents_list.dart';
 
 class HomePage extends StatelessWidget {
   static const String routeName = Navigator.defaultRouteName;
@@ -13,17 +17,51 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
       children: <Widget>[
-        LimitedContentsListPanel(
-          stream: HomeBloc.instance.articleStream.stream,
-          type: ContentsType.ARTICLE,
+        PanelHeaderView(
+          title: 'Articles',
+          sideWidget: _buildShowMore(
+            context,
+            ContentsListPage.routeNameArticles,
+          ),
+        ),
+        MinimizedContentsList(
+          ArticleItemView.builder,
+          'articles',
+          maxCount: 3,
+          separator: SizedBox(
+            height: 16,
+          ),
         ),
         SizedBox(height: 18),
-        LimitedContentsListPanel(
-          stream: HomeBloc.instance.questionStream.stream,
-          type: ContentsType.QUESTION,
+        PanelHeaderView(
+          title: 'Questions',
+          sideWidget: _buildShowMore(
+            context,
+            ContentsListPage.routeNameQuestions,
+          ),
+        ),
+        MinimizedContentsList(
+          QuestionItemView.builder,
+          'questions',
+          maxCount: 3,
         ),
       ],
+    );
+  }
+
+  Widget _buildShowMore(
+    BuildContext context,
+    String route, {
+    String query = '',
+  }) {
+    return InkWell(
+      onTap: () => WebNavigator.of(context).pushNamed('$route$query'),
+      child: Text(
+        'more >',
+        style: Theme.of(context).textTheme.caption,
+      ),
     );
   }
 }
