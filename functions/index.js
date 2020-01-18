b5edf802c90eae4cad67671d466744ec8dce48cf
Huan Suh <huansuh@gmail.com>
2020-01-18 13:30:13 +0900

    Contents Apis
     + articles/questions : on comments/likes updated

--- functions/index.js
+++ functions/index.js
@@ -5,7 +5,47 @@ admin.initializeApp();
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-    response.set("Access-Control-Allow-Origin", "http://localhost:8686");
-    response.send("Hello from Firebase!");
-});
+exports.onArticlesChildUpdate = functions.firestore
+  .document('articles/{articleId}/{type}/{itemId}')
+  .onWrite((change, context) => {
+    const db = admin
+      .firestore()
+      .collection('articles')
+      .doc(context.params.articleId);
+    db.collection(context.params.type)
+      .get()
+      .then(snapshot => {
+        const count =
+          snapshot && snapshot.size && !isNaN(snapshot.size)
+            ? snapshot.size
+            : 0;
+        if(context.params.type == 'comments') {
+            db.update({ commentCount: count });
+        } else if(context.params.type == 'likes') {
+            db.update({ likeCount: count });
+        }
+      });
+  });
+
+
+exports.onQuestionsChildUpdate = functions.firestore
+  .document('questions/{articleId}/{type}/{itemId}')
+  .onWrite((change, context) => {
+    const db = admin
+      .firestore()
+      .collection('questions')
+      .doc(context.params.articleId);
+    db.collection(context.params.type)
+      .get()
+      .then(snapshot => {
+        const count =
+          snapshot && snapshot.size && !isNaN(snapshot.size)
+            ? snapshot.size
+            : 0;
+        if(context.params.type == 'comments') {
+            db.update({ commentCount: count });
+        } else if(context.params.type == 'likes') {
+            db.update({ likeCount: count });
+        }
+      });
+  });
