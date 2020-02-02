721a0f269d3dbf9aff9eef0c589d7ebc7242350d
Huan Suh <huansuh@gmail.com>
2020-02-02 15:43:26 +0900

    resolve NPE

--- lib/src/view/component/contents/view_contents_userInfo.dart
+++ lib/src/view/component/contents/view_contents_userInfo.dart
@@ -1,13 +1,5 @@
 import 'package:intl/intl.dart';
-import 'package:provider/provider.dart';
-import 'package:stackoverflutter/src/bloc/contents_detail_bloc.dart';
-import 'package:stackoverflutter/src/bloc/user_bloc.dart';
-import 'package:stackoverflutter/src/bloc/user_detail_bloc.dart';
-import 'package:stackoverflutter/src/model/contents/article_item.dart';
 import 'package:stackoverflutter/src/model/contents/contents_item.dart';
-import 'package:stackoverflutter/src/model/user/user_item.dart';
-import 'package:stackoverflutter/src/view/component/contents/view_contents_viewer.dart';
-import 'package:stackoverflutter/src/view/component/view_user_profile.dart';
 import 'package:flutter/material.dart';
 
 class ContentsUserInfoView extends StatelessWidget {
@@ -17,11 +9,11 @@ class ContentsUserInfoView extends StatelessWidget {
 
   final ContentsItem passedItem;
 
-  const ContentsUserInfoView(this._contentsType, {
+  const ContentsUserInfoView(
+    this._contentsType, {
     this.passedItem,
     Key key,
   }) : super(key: key);
-
 
   @override
   Widget build(BuildContext context) {
@@ -46,11 +38,12 @@ class ContentsUserInfoView extends StatelessWidget {
                       passedItem?.userId ?? 'userId404',
                       style: TextStyle(fontSize: 16),
                     ),
-                    Text(
-                      DateFormat('yyyy.MM.dd HH:mm')
-                          .format(passedItem?.createTime),
-                      style: TextStyle(fontSize: 10),
-                    ),
+                    if (passedItem?.createTime != null)
+                      Text(
+                        DateFormat('yyyy.MM.dd HH:mm')
+                            .format(passedItem?.createTime),
+                        style: TextStyle(fontSize: 10),
+                      ),
                   ],
                 ),
               ],
@@ -60,8 +53,7 @@ class ContentsUserInfoView extends StatelessWidget {
                 IconButton(
                   icon: Icon(Icons.favorite_border),
                 ),
-                Text(passedItem?.likeCount.toString()?? '404'),
-
+                Text('${passedItem?.likeCount ?? 0}'),
               ],
             ),
           ],
